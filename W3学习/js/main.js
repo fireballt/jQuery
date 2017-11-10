68debbaf4fb3a706605603f8cf41c657d75ee49c
fireballt <30947603+fireballt@users.noreply.github.com>
2017-11-10 17:25:39 +0800

    完成

---
+++ W3学习/js/main.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+
+	var $nav = $("#nav");
+	var $menu = $("#menu");
+	$nav.hide();
+
+	$nav.animate({right:'-210px'});
+
+	$menu.click(function(){
+		// alert("aa");
+	$nav.show();
+	$nav.animate({right:'0px'});
+	$(this).hide();	
+	});
+	$("#nav a").click(function(){
+		$nav.animate({right:'-210px'});
+		$menu.fadeIn();
+	})
+	// $("menu").an
+
+});
